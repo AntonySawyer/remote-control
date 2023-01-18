a7fa4b9afab550961039a4697bf3364ab645682b
Antony <a.krutenok@gmail.com>
2023-01-18 11:19:25 +0300

    feat: Added eslint and related scripts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
 import * as dotenv from 'dotenv';
+
+import { httpServer } from './src/http_server/index';
 
 import { initWebsocketServer } from './src/ws_server/index';
 
@@ -14,8 +14,8 @@ const {
   WS_PORT = DEFAULT_WS_PORT,
 } = process.env;
 
+// eslint-disable-next-line no-console
 console.log(`Start static http server on the ${STATIC_CONTENT_PORT} port!`);
 httpServer.listen(STATIC_CONTENT_PORT);
-
 
 initWebsocketServer(Number(WS_PORT));
--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -1,11 +1,8 @@
-import { WebSocketServer } from "ws";
-
+import { WebSocketServer } from 'ws';
 
 export const initWebsocketServer = (port: number) => {
-
   const wss = new WebSocketServer({ port });
-
   wss.on('connection', () => {
-    console.log('connected')
+    console.log('connected');
   });
-}
+};
